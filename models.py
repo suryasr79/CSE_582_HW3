201fcccde262f4258c2813cef0b811166cb18103
suryasr79 <suryasr79@gmail.com>
2024-02-20 22:09:34 -0500

    🚧| CSE_582_HW3 | Add Implementation

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import random
 from torch.autograd import Variable as Var
+from torch import optim
 from utils import *
 from data import *
 from lf_evaluator import *
@@ -60,18 +61,36 @@ class NearestNeighborSemanticParser(object):
         return test_derivs
 
 class Seq2SeqSemanticParser(nn.Module):
-    def __init__(self, input_indexer, output_indexer, emb_dim, hidden_size, embedding_dropout=0.2, bidirect=True):
+    def __init__(self, input_indexer, output_indexer, emb_dim, hidden_size, model_input_emb, model_enc, model_output_emb, model_dec, embedding_dropout=0.2, decoder_len_limit = 65, bidirect=True):
         # We've include some args for setting up the input embedding and encoder
         # You'll need to add code for output embedding and decoder
         super(Seq2SeqSemanticParser, self).__init__()
         self.input_indexer = input_indexer
         self.output_indexer = output_indexer
+        self.decoder_len_limit = decoder_len_limit
         
         self.input_emb = EmbeddingLayer(emb_dim, len(input_indexer), embedding_dropout)
         self.encoder = RNNEncoder(emb_dim, hidden_size, bidirect)
-        raise Exception("implement me!")
 
-    def forward(self, x_tensor, inp_lens_tensor, y_tensor, out_lens_tensor):
+        self.model_input_emb = model_input_emb
+        self.model_enc = model_enc
+        self.model_output_emb = model_output_emb
+        self.model_dec = model_dec
+
+        self.hidden_size = hidden_size
+        self.rnn = nn.LSTM(hidden_size, hidden_size, num_layers=2, batch_first=True,dropout=0.2)
+        self.ff = nn.Linear(hidden_size, len(output_indexer))
+        self.softmax = nn.LogSoftmax(dim = 1)
+        self.init_weight()
+
+    def init_weight(self):
+        nn.init.xavier_uniform_(self.rnn.weight_hh_l0, gain=1)
+        nn.init.xavier_uniform_(self.rnn.weight_ih_l0, gain=1)
+        nn.init.constant_(self.rnn.bias_hh_l0, 0)
+        nn.init.constant_(self.rnn.bias_ih_l0, 0)
+        nn.init.xavier_uniform_(self.ff.weight)
+
+    def forward(self, ex, x_tensor, inp_lens_tensor, y_tensor, out_lens_tensor, teacher_forcing_ratio, model_enc, model_dec, model_input_emb, model_output_emb, enc_optimizer, dec_optimizer, input_emb_optimizer, output_emb_optimizer):
         """
         :param x_tensor/y_tensor: either a non-batched input/output [sent len] vector of indices or a batched input/output
         [batch size x sent len]. y_tensor contains the gold sequence(s) used for training
@@ -79,12 +98,99 @@ class Seq2SeqSemanticParser(nn.Module):
         lengths aren't needed if you don't batchify the training.
         :return: loss of the batch
         """
-        raise Exception("implement me!")
+        model_enc.train()
+        model_dec.train()
+        model_input_emb.train()
+        model_output_emb.train()
+
+        model_enc.zero_grad()
+        model_dec.zero_grad()
+        model_input_emb.zero_grad()
+        model_output_emb.zero_grad()
+
+        loss = 0
+        SOS_token = 1
+        EOS_token = 2
+        criterion = torch.nn.CrossEntropyLoss()
+
+        (enc_output, enc_context_mask, enc_hidden, enc_bi_hidden) = self.encode_input(x_tensor, inp_lens_tensor, model_input_emb, model_enc)
+        dec_hidden = enc_hidden
+        input_output_map = create_input_output_map(ex.x_indexed, self.input_indexer, self.output_indexer)
+        context_vec = enc_bi_hidden[0]
+
+        dec_input = torch.as_tensor([[SOS_token]])
+        y_temp = []
+
+        teacher_forcing = True if random.random() <= teacher_forcing_ratio else False
+
+        for y_ex in range(len(y_tensor)):
+            dec_output, dec_input, dec_input_val, dec_hidden, context_vec, for_inference = decode_attn(dec_input, dec_hidden, model_output_emb, model_dec, context_vec, enc_output, 1, input_output_map)
+            loss += criterion(dec_output, y_tensor[y_ex].unsqueeze(0))
+            y_temp.append(dec_input.item())
+            if dec_input == EOS_token:
+                break
+            if teacher_forcing:
+                dec_input = y_tensor[y_ex].unsqueeze(0).unsqueeze(0)
+
+        loss.backward()
+        input_emb_optimizer.step()
+        output_emb_optimizer.step()
+        enc_optimizer.step()
+        dec_optimizer.step()
+
+        return loss
 
     def decode(self, test_data: List[Example]) -> List[List[Derivation]]:
-        raise Exception("implement me!")
+        self.model_input_emb.eval()
+        self.model_enc.eval()
+        self.model_output_emb.eval()
+        self.model_dec.eval()
 
-    def encode_input(self, x_tensor, inp_lens_tensor):
+        self.model_input_emb.zero_grad()
+        self.model_output_emb.zero_grad()
+        self.model_enc.zero_grad()
+        self.model_dec.zero_grad()
+
+        SOS_token = 1
+        EOS_token = self.output_indexer.index_of('<EOS>')
+        beam_length = 1
+        derivations = []
+
+        for ex in test_data:
+            count = 0
+            y_toks =[]
+            self.model_input_emb.zero_grad()
+            self.model_output_emb.zero_grad()
+            self.model_enc.zero_grad()
+            self.model_dec.zero_grad()
+
+            x_tensor = torch.as_tensor([ex.x_indexed])
+            y_tensor = torch.as_tensor(ex.y_indexed)
+            inp_lens_tensor = torch.as_tensor([len(ex.x_indexed)])
+
+            enc_output, enc_context_mask, enc_hidden, enc_bi_hidden = self.encode_input(x_tensor, inp_lens_tensor, self.model_input_emb, self.model_enc)
+            dec_hidden = enc_hidden
+            context_vec = enc_bi_hidden[0]
+            dec_input = torch.as_tensor([[SOS_token]])
+            input_output_map = create_input_output_map(ex.x_indexed, self.input_indexer, self.output_indexer)
+
+            while (dec_input.item() != EOS_token) and count <= self.decoder_len_limit:
+                dec_output, dec_input, dec_input_val, dec_hidden, context_vec, for_inference = decode_attn(dec_input, dec_hidden, self.model_output_emb, self.model_dec, context_vec, enc_output, beam_length, input_output_map)
+                top_val, top_ind = for_inference.topk(1)
+                top_ind = top_ind.detach()
+                if top_ind < len(self.output_indexer):
+                    y_label = self.output_indexer.get_object(dec_input.item())
+                else:
+                    source_word_idx = top_ind.item() - len(self.output_indexer)
+                    copy_word_idx = ex.x_indexed[source_word_idx]
+                    y_label = self.input_indexer.get_object(copy_word_idx)
+                if dec_input.item() != EOS_token:
+                    y_toks.append(y_label)
+                count = count + 1
+            derivations.append([Derivation(ex, 1.0, y_toks)])
+        return derivations
+
+    def encode_input(self, x_tensor, inp_lens_tensor, model_input_emb, model_enc):
         """
         Runs the encoder (input embedding layer and encoder as two separate modules) on a tensor of inputs x_tensor with
         inp_lens_tensor lengths.
@@ -108,11 +214,10 @@ class Seq2SeqSemanticParser(nn.Module):
         enc_output_each_word = 3 x 3 x dim, enc_context_mask = [[1, 1, 0], [1, 1, 1], [1, 0, 0]],
         enc_final_states = 3 x dim
         """
-        input_emb = self.input_emb.forward(x_tensor)
-        (enc_output_each_word, enc_context_mask, enc_final_states) = self.encoder.forward(input_emb, inp_lens_tensor)
+        input_emb = model_input_emb.forward(x_tensor)
+        (enc_output_each_word, enc_context_mask, enc_final_states, enc_bi_hidden) = model_enc.forward(input_emb, inp_lens_tensor)
         enc_final_states_reshaped = (enc_final_states[0].unsqueeze(0), enc_final_states[1].unsqueeze(0))
-        return (enc_output_each_word, enc_context_mask, enc_final_states_reshaped)
-
+        return (enc_output_each_word, enc_context_mask, enc_final_states_reshaped, enc_bi_hidden)
 
 class EmbeddingLayer(nn.Module):
     """
@@ -154,6 +259,7 @@ class RNNEncoder(nn.Module):
         super(RNNEncoder, self).__init__()
         self.bidirect = bidirect
         self.hidden_size = hidden_size
+        self.input_emb_dim = input_emb_dim
         self.reduce_h_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
         self.reduce_c_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
         self.rnn = nn.LSTM(input_emb_dim, hidden_size, num_layers=1, batch_first=True,
@@ -191,13 +297,67 @@ class RNNEncoder(nn.Module):
             h_, c_ = torch.cat((h[0], h[1]), dim=1), torch.cat((c[0], c[1]), dim=1)
             # Reduce them by multiplying by a weight matrix so that the hidden size sent to the decoder is the same
             # as the hidden size in the encoder
+            bi_ht = (h_, c_)
             new_h = self.reduce_h_W(h_)
             new_c = self.reduce_c_W(c_)
             h_t = (new_h, new_c)
         else:
             h, c = hn[0][0], hn[1][0]
             h_t = (h, c)
-        return (output, context_mask, h_t)
+        return (output, context_mask, h_t, bi_ht)
+
+class AttnRNNDecoder(nn.Module):
+    def __init__(self, input_size, hidden_size_enc, hidden_size_dec, out):
+        super(AttnRNNDecoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size_enc = hidden_size_enc
+        self.hidden_size_dec = hidden_size_dec
+        self.out = out
+        self.rnn = nn.LSTM(input_size, hidden_size_dec, num_layers=1, batch_first=True, dropout=0)
+        self.attn = nn.Linear(hidden_size_enc, hidden_size_dec)
+        self.attn_hid = nn.Linear(hidden_size_dec + hidden_size_enc, hidden_size_dec)
+        self.ff = nn.Linear(hidden_size_dec, out)
+        self.softmax = nn.Softmax(dim = 1)
+        self.Wh = nn.Linear(hidden_size_enc, 1)
+        self.Ws = nn.Linear(hidden_size_dec, 1)
+        self.Wx = nn.Linear(input_size,1)
+        self.bptr = nn.Linear(1,1)
+        self.sigmoid = nn.Sigmoid()
+        self.init_weight()
+
+    def init_weight(self):
+        nn.init.xavier_uniform_(self.rnn.weight_hh_l0, gain=1)
+        nn.init.xavier_uniform_(self.rnn.weight_ih_l0, gain=1)
+        nn.init.constant_(self.rnn.bias_hh_l0, 0)
+        nn.init.constant_(self.rnn.bias_ih_l0, 0)
+        nn.init.xavier_uniform_(self.attn.weight)
+        nn.init.xavier_uniform_(self.attn_hid.weight)
+        nn.init.xavier_uniform_(self.ff.weight)
+        nn.init.xavier_uniform_(self.Wh.weight)
+        nn.init.xavier_uniform_(self.Ws.weight)
+        nn.init.xavier_uniform_(self.Wx.weight)
+        nn.init.xavier_uniform_(self.bptr.weight)
+
+    def forward(self, input, hidden, encoder_outputs, input_output_map):
+        output, (h,c) = self.rnn(input, hidden)
+        h_bar = h[0]
+        encoder_outputs = encoder_outputs.squeeze()
+        attn_weight = self.attn(encoder_outputs).squeeze()
+        attn_weight = torch.transpose(attn_weight, 0, 1)
+        attn_energy = torch.matmul(h_bar, attn_weight)
+        attn_score = F.softmax(attn_energy, dim = 1)
+        context = torch.matmul(attn_score, encoder_outputs)
+        attn_hid_combined = torch.cat((context, h_bar), 1)
+        attn_hid_transformed = self.attn_hid(attn_hid_combined)
+        out = self.ff(attn_hid_transformed)
+        p_vocab = self.softmax(out)
+        p_gen = self.sigmoid(self.Wh(context) + self.Ws(h_bar))
+        p_gen = p_gen.squeeze(0)
+        ai = torch.matmul(attn_score, input_output_map)
+        Pw = p_gen * p_vocab + (1-p_gen) * ai
+        Pw = torch.log(Pw)
+        for_inference = torch.cat((p_gen*p_vocab, (1-p_gen)*attn_score), 1)
+        return Pw, (h,c), context, for_inference
 
 def make_padded_input_tensor(exs: List[Example], input_indexer: Indexer, max_len: int, reverse_input=False) -> np.ndarray:
     """
@@ -230,6 +390,27 @@ def make_padded_output_tensor(exs, output_indexer, max_len):
     """
     return np.array([[ex.y_indexed[i] if i < len(ex.y_indexed) else output_indexer.index_of(PAD_SYMBOL) for i in range(0, max_len)] for ex in exs])
 
+def decode_attn(y_index, hidden, model_output_emb, model_dec, context_vec, encoder_output, beam_length, input_output_map):
+    output_emb = model_output_emb.forward(y_index)
+    output_emb= torch.cat((output_emb.squeeze(1), context_vec), dim = 1)
+    output_emb = output_emb.unsqueeze(0)
+    output, hidden, context_vec, for_inference = model_dec.forward(output_emb, hidden, encoder_output, input_output_map)
+    top_val, top_ind = output.topk(beam_length)
+    dec_input = top_ind.detach()
+    dec_input_prob = top_val.detach()
+    return output, dec_input, dec_input_prob, hidden, context_vec, for_inference
+
+def create_input_output_map(input_seq, input_indexer, output_indexer):
+    map = torch.zeros(len(input_seq), len(output_indexer))
+    unk_index = output_indexer.index_of(UNK_SYMBOL)
+    for i, idx in enumerate(input_seq):
+        word = input_indexer.get_object(idx)
+        out_index = output_indexer.index_of(word)
+        if out_index == -1:
+            map[i][unk_index] =  1.0
+        else:
+            map[i][out_index] =  1.0
+    return map
 
 def train_model_encdec(train_data: List[Example], dev_data: List[Example], input_indexer, output_indexer, args) -> Seq2SeqSemanticParser:
     """
@@ -257,4 +438,40 @@ def train_model_encdec(train_data: List[Example], dev_data: List[Example], input
 
     # First create a model. Then loop over epochs, loop over examples, and given some indexed words
     # call your seq-to-seq model, accumulate losses, update parameters
-    raise Exception("Implement the rest of me to train your encoder-decoder model")
+    emb_dim = 300
+    hidden_size = 200
+    teacher_forcing_ratio = 1.0
+    epochs = 30
+
+    model_input_emb = EmbeddingLayer(emb_dim, len(input_indexer), 0.2)
+    model_output_emb = EmbeddingLayer(emb_dim, len(output_indexer), 0.2)
+
+    model_enc = RNNEncoder(emb_dim, hidden_size, True)
+    model_dec = AttnRNNDecoder(emb_dim + hidden_size*2, hidden_size * 2, hidden_size, len(output_indexer))
+
+    decoder = Seq2SeqSemanticParser(input_indexer, output_indexer, emb_dim, hidden_size, model_input_emb, model_enc, model_output_emb, model_dec)
+
+    enc_optimizer = optim.AdamW(model_enc.parameters(), lr=.001)
+    dec_optimizer = optim.AdamW(model_dec.parameters(), lr=.001)
+
+    input_emb_optimizer = optim.AdamW(model_input_emb.parameters(), lr=args.lr)
+    output_emb_optimizer = optim.AdamW(model_output_emb.parameters(), lr=args.lr)
+
+    for i in range(epochs):
+        count = 0
+        teacher_forcing_ratio = teacher_forcing_ratio**i
+        for ex in train_data:
+            x_tensor = torch.as_tensor([ex.x_indexed])
+            y_tensor = torch.as_tensor(ex.y_indexed)
+            inp_lens_tensor = torch.as_tensor([len(ex.x_indexed)])
+            out_lens_tensor = torch.as_tensor([len(ex.y_indexed)])
+
+            loss = decoder(ex, x_tensor, inp_lens_tensor, y_tensor, out_lens_tensor, teacher_forcing_ratio, model_enc, model_dec, model_input_emb, model_output_emb, enc_optimizer, dec_optimizer, input_emb_optimizer, output_emb_optimizer)
+
+            if count %25 == 0:
+                print ("The loss is: ", loss)
+            count += 1
+
+        print("Current epoch is: ", i)
+
+    return decoder
